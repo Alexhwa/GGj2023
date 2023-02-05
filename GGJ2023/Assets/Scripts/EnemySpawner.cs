e10bf4f213dd48bfa24a2ee3693e44db3d1becd8
Alexhwa <ah.alswad@gmail.com>
2023-02-05 05:59:01 -0600

    spawner behavior setup

---
+++ GGJ2023/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public UnityEvent onSpawn = new UnityEvent();
+    public float elapsedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
